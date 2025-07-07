efadf9e0980da3ce545c75e4f0a48668d8831509
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-07 18:09:42 +0200

    [add] feat esercizio palindroma: function Palindroma .toLowerCase

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 //Palindroma
 //definizione variabili
 const parola = prompt('Inserisci una parola');
+
+function Palindroma() {
+  let minuscola = parola.toLowerCase();
+  console.log(minuscola);
+}
+Palindroma();
